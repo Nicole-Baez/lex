fe1a7d8c3786ab3c2ec5d9a83a4e9684ed9b4bec
Yedelmys Baez <nicolebaezesp@gmail.com>
2026-02-21 22:37:45 -0500

    empece el while loop y declare unos cuantos arrays

--- lex.c
+++ lex.c
@@ -22,13 +22,23 @@ Instructor: Dr. Jie Lin
 Due Date: Monday, March 3, 2026
 */
 
+
+
+
+
+
+
+
+
+
 //structure for enumeration
 typedef enum {
     skipsym = 1 , // Skip / ignore token
     identsym = 2, // Identifier
     numbersym = 3, // Number
     beginsym = 4, // begin
-    endsym = 5, // if
+    endsym = 5, // end
+    ifsym = 6, //if
     fisym = 7, // fi
     thensym = 8, // then
     whilesym = 9, // while
@@ -56,8 +66,8 @@ typedef enum {
     commasym = 31, // ,
     semicolonsym = 32, // ;
     periodsym = 33, // .
-    becomessym = 34 // :=
-} TokenType ;
+    becomessym = 34, // :=
+} TokenType;
 
 
 
@@ -66,13 +76,25 @@ typedef enum {
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define norw 15; //num of reserved words
-#define imax 99999; //max num of integer value(can only be 5 digits long)
-#define cmax 11; //identifier length
-#define strmax 256; //str max length
+#define norw 15 //num of reserved words
+#define imax 99999 //max num of integer value(can only be 5 digits long)
+#define cmax 11 //identifier length
+#define strmax 256 //str max length
+
+/*
+    Error messages:
+    1- Identifier too long
+    2- Number too long
+    3- Invalid symbols (idk how to check this)
 
 
+*/
+
+//function that maps reserved words
+
+//function that maps special symbols
 
 
 int main(int argc, char *argv[])
@@ -81,27 +103,85 @@ int main(int argc, char *argv[])
     char *reservedWord[] = {"null", "begin", "call", "const", "do", "else", "end", "if", "odd", "procedure", "read", "then",
     "var", "while", "write"};
 
-    char *identifier[] = ""; //to store the identifier name (to check if length exceeds)
+    
+    
+    //char *identifier[] = ""; //to store the identifier name (to check if length exceeds)
+
+    char *groupLexemes[] = {""};
 
 
-    char *nameTable[] = {""}; //to store the identifier
+    char *nameTable[] = {""}; //to store the name table
 
-    char ch; //to read each char
+    int *tokenList[] = {0}; // to store all the tokens 
+
+    char ch = ""; //to read each char
 
     
-    if (argc > 1)
+    if (argc == 2)
     {
         FILE *ip = fopen(argv[1], "r");
 
-        printf("Source Program");
+        printf("Source Program:\n");
+        int i = 0;
+        int commentCheck = 0;
+        
+        while (ch = fgetc(ip) != EOF)
+        {
+            
+            //ignores comments
+            if (ch == '/*'){
+                commentCheck = 1;
+                continue;
+            }
 
-        while(fscanf(ip, "%c", &))
+            if(commentCheck)
+            {
+                if(ch == '*/'){
+                    commentCheck = 0;
+                }
+
+                continue;
+            }
+
+
+            /*For printing purposes. It prints the source code just as is. */
+             if (ch == ' '){
+                printf(" ");
+             }
+             
+             if (ch == '\t'){
+                printf("\t");
+             }
+
+             if (ch == '\n'){
+                printf("\n");
+             }
+
+             if (ch == '\r'){
+                printf("\r");
+             }
+
+             
+            printf("%c", ch);
+
+            if (ch != ' ' || ch != '\t' || ch != '\n' || ch != '\r')
+            {
+                groupLexemes[i] = ch;
+            }
+
+        }
+
+
+        fclose(ip);
 
 
 
+    }
 
-
-
+    else if(argc > 2){
+        printf("Extra arguments were provided.\n");
+        printf("Try: ./00_args 123\n");
+        
     }
     else
     {
